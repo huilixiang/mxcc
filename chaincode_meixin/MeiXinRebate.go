96849137f5e11f4fee47366501f036bb92427e4b
hua jie <huajie.cqupt@gmail.com>
2016-09-02 16:49:36 +0800

    init

--- chaincode_meixin/MeiXinRebate.go
+++ chaincode_meixin/MeiXinRebate.go
@@ -15,8 +15,8 @@ import (
 	"github.com/op/go-logging"
 )
 
-var shareTxPrefix = "USTX_"
-var downloadTxPrefix = "UDTX_"
+var shareTXPrefix = "USTX_"
+var downloadTXPrefix = "UDTX_"
 var payerPrefix = "PAYER_"
 var payeePrefix = "PAYEE_"
 
@@ -85,13 +85,72 @@ func (r *RebateChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 		rebateLogger.Warningf("payer:[%s]`s balance:[%d]", payerID, balance)
 		return []byte(strconv.Itoa(balance)), nil
 	} else if "payeeBalance" == function {
+		payeeID := args[0]
+		balance, err := r.getPayeeBalance(ccStub, payeeID)
+		if err != nil {
+			return nil, err
+		}
+		rebateLogger.Debugf("payee:[%s]`s balance:[%d]", payeeID, balance)
+		return []byte(strconv.Itoa(balance)), nil
+	} else if "shareTX" == function {
+		txID := args[0]
+		txBytes, err := stub.GetState(shareTXPrefix + txID)
+		if err != nil {
+			return nil, err
+		}
+		rebateLogger.Debugf("share tx:[%s]`s detail:[%s].", txID, string(txBytes))
+		return txBytes, nil
+	} else if "downloadTX" == function {
+		txID := args[0]
+		txBytes, err := stub.GetState(downloadTXPrefix + txID)
+		if err != nil {
+			return nil, err
+		}
+		rebateLogger.Debugf("download tx:[%s]`s detail:[%s].", txID, string(txBytes))
+		return txBytes, nil
 
 	}
-
 	return nil, nil
 }
 
 func (r *RebateChaincode) download(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+
+	if len(args) != 5 {
+		return nil, errors.New("Incorrect number of arguments. Exception 5")
+	}
+	uid := args[0]
+	actType, err := strconv.Atoi(args[1])
+	if err != nil {
+		return nil, fmt.Errorf("Error parsing actType:[%s]", args[1])
+	}
+	app := args[2]
+	bonusType, err := strconv.Atoi(args[3])
+	if err != nil {
+		return nil, fmt.Errorf("Error parsing bonusType:[%s]", args[3])
+	}
+	prevTXID := args[4]
+	actionInfo := ActionInfo{Uid: uid, PrevTx: prevTXID, BonusType: bonusType, App: app, ActType: actType}
+	curTXID := stub.UUID
+	stateKey := downloadTXPrefix + curTXID
+	jsonStr, err := json.Marshal(actionInfo)
+	if err != nil {
+		return nil, fmt.Errorf("Json.Marshal actionInfo error")
+	}
+	err = stub.PutState(stateKey, []byte(jsonStr))
+	if err != nil {
+		return nil, fmt.Errorf("stub.PutState error: [%s]", err)
+	}
+	rebateLogger.Debugf("share success, json:[%s]", jsonStr)
+	//调用返利处理函数。此处只是demo阶段，应该有一条单独的链来做这项工作
+	callArgs := make([]string, 3)
+	//应该提供app与软件供应商的对应关系，找到支付人，此处简单的模拟
+	callArgs[0] = curTXID
+	callArgs[1] = fmt.Sprintf("%d", LastFour)
+	callArgs[2] = "payer_0"
+	_, err = r.rebate(stub, callArgs)
+	if err != nil {
+		return nil, err
+	}
 	return nil, nil
 }
 
@@ -112,7 +171,7 @@ func (r *RebateChaincode) share(stub *shim.ChaincodeStub, args []string) ([]byte
 	prevTXID := args[4]
 	actionInfo := ActionInfo{Uid: uid, PrevTx: prevTXID, BonusType: bonusType, App: app, ActType: actType}
 	curTXID := stub.UUID
-	stateKey := shareTxPrefix + curTXID
+	stateKey := shareTXPrefix + curTXID
 	jsonStr, err := json.Marshal(actionInfo)
 	if err != nil {
 		return nil, fmt.Errorf("Json.Marshal actionInfo error")
@@ -149,9 +208,7 @@ func (r *RebateChaincode) payerDeposit(stub *shim.ChaincodeStub, args []string) 
 	balance += amount
 	err = stub.PutState(payerPrefix+payerID, []byte(strconv.Itoa(balance)))
 	if err != nil {
-		warning := fmt.Sprintf("payer:[%s] deposit failed, err:[%s]", payerID, err)
-		rebateLogger.Warningf(warning)
-		return nil, errors.New(warning)
+		return nil, fmt.Errorf("payer:[%s] deposit failed, err:[%s]", payerID, err)
 	}
 	return []byte(strconv.Itoa(balance)), nil
 }
@@ -162,7 +219,7 @@ func (r *RebateChaincode) rebate(stub *shim.ChaincodeStub, args []string) ([]byt
 	if len(args) != 3 {
 		return nil, errors.New("Incorrect number of arguments. Excepting 3")
 	}
-	downloadTxID := args[0]
+	downloadTXID := args[0]
 	bonusType, err := strconv.Atoi(args[1])
 	payerID := args[2]
 	var payees []string
@@ -172,10 +229,10 @@ func (r *RebateChaincode) rebate(stub *shim.ChaincodeStub, args []string) ([]byt
 		break
 	case LastFour:
 		//分享链上的最后四个人
-		payees, err = r.findLastN(stub, downloadTxID, 4)
+		payees, err = r.findLastN(stub, downloadTXID, 4)
 		break
 	default:
-		return nil, errors.New(fmt.Sprintf("there is no matched BonusType:[%d]", bonusType))
+		return nil, fmt.Errorf("there is no matched BonusType:[%d]", bonusType)
 	}
 	balance, err := r.getPayerBalance(stub, payerID)
 	if err != nil {
@@ -183,6 +240,9 @@ func (r *RebateChaincode) rebate(stub *shim.ChaincodeStub, args []string) ([]byt
 	}
 	rebateMoney := 10
 	for _, payee := range payees {
+		if len(payee) < 1 {
+			break
+		}
 		rebateLogger.Warningf("rebate user:[%s], money:[%d]", payee, 10)
 		//给用户转帐
 		payeeKey := payeePrefix + payee
@@ -194,6 +254,9 @@ func (r *RebateChaincode) rebate(stub *shim.ChaincodeStub, args []string) ([]byt
 		payeeBalance := 0
 		if payeeBalanceBytes != nil {
 			payeeBalance, err = strconv.Atoi(string(payeeBalanceBytes))
+			if err != nil {
+				return nil, err
+			}
 		}
 		balance = balance - rebateMoney
 		payeeBalance = payeeBalance + rebateMoney
@@ -202,7 +265,9 @@ func (r *RebateChaincode) rebate(stub *shim.ChaincodeStub, args []string) ([]byt
 			rebateLogger.Warningf("payer:[%s] rebate:[%d] to payee:[%s] err:[%s]", payerID, rebateMoney, payee, err)
 			return nil, err
 		}
+		rebateLogger.Debugf("payer:[%s] rebate:[%d] to payee:[%s], balance:[%d]", payerID, rebateMoney, payee, balance)
 	}
+
 	err = stub.PutState(payerPrefix+payerID, []byte(strconv.Itoa(balance)))
 	if err != nil {
 		return nil, err
@@ -210,14 +275,28 @@ func (r *RebateChaincode) rebate(stub *shim.ChaincodeStub, args []string) ([]byt
 	return nil, nil
 }
 
+func (r *RebateChaincode) getPayeeBalance(stub *shim.ChaincodeStub, payeeID string) (int, error) {
+	var key = payeePrefix + payeeID
+	balanceBytes, err := stub.GetState(key)
+	if err != nil {
+		return -1, fmt.Errorf("getting payee:[%s]`s balance err:[%s]", payeeID, err)
+	}
+	balance := 0
+	if balanceBytes != nil {
+		balance, err = strconv.Atoi(string(balanceBytes))
+		if err != nil {
+			return -1, fmt.Errorf("parsing payee:[%s]`s balance:[%s] err:[%s]", payeeID, string(balanceBytes), err)
+		}
+	}
+	return balance, nil
+}
+
 func (r *RebateChaincode) getPayerBalance(stub *shim.ChaincodeStub, payerID string) (int, error) {
 
 	var payerKey = payerPrefix + payerID
 	balanceBytes, err := stub.GetState(payerKey)
 	if err != nil {
-		warning := fmt.Sprintf("GetState failed for payer:[%s] deposit, err:[%s]", payerID, err)
-		rebateLogger.Warningf(warning)
-		return -1, errors.New(warning)
+		return -1, fmt.Errorf("GetState failed for payer:[%s] deposit, err:[%s]", payerID, err)
 	}
 	balance := 0
 	if balanceBytes == nil {
@@ -225,9 +304,7 @@ func (r *RebateChaincode) getPayerBalance(stub *shim.ChaincodeStub, payerID stri
 	} else {
 		balance, err = strconv.Atoi(string(balanceBytes))
 		if err != nil {
-			warning := fmt.Sprintf("payer:[%s]`s balance[%s] is abnormal", payerID, string(balanceBytes))
-			rebateLogger.Warningf(warning)
-			return -1, errors.New(warning)
+			return -1, fmt.Errorf("payer:[%s]`s balance[%s] is abnormal", payerID, string(balanceBytes))
 		}
 	}
 	return balance, nil
@@ -239,13 +316,14 @@ func (r *RebateChaincode) findTopN(stub *shim.ChaincodeStub, rightTxID string, n
 
 func (r *RebateChaincode) findLastN(stub *shim.ChaincodeStub, rightTxID string, n int) ([]string, error) {
 	var payees = make([]string, n)
-	key := downloadTxPrefix + rightTxID
+	key := downloadTXPrefix + rightTxID
 	for i := 0; i < n; {
 		stateBytes, err := stub.GetState(key)
 		if err != nil {
 			rebateLogger.Warningf("GetState by key:[%s] err:[%s]", key, err)
 			continue
 		}
+
 		//stateStr := string(stateBytes)
 		var ai ActionInfo
 		err = json.Unmarshal(stateBytes, &ai)
@@ -256,7 +334,10 @@ func (r *RebateChaincode) findLastN(stub *shim.ChaincodeStub, rightTxID string, 
 		prevTx := ai.PrevTx
 		uid := ai.Uid
 		payees[i] = uid
-		key = shareTxPrefix + prevTx
+		if len(prevTx) < 1 {
+			break
+		}
+		key = shareTXPrefix + prevTx
 		i++
 	}
 	return payees, nil
